303354f5b279b8a2a56c45aa62ba08a2e38b33cd
anngo-1 <andngo2004@gmail.com>
2024-12-02 16:37:45 -0800

    fix

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from works import work_data_bp
-
+from journals import journal_data_bp
 
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(work_data_bp, url_prefix="/works")
-
+app.register_blueprint(journal_data_bp, url_prefix="/works")
 
 
 if __name__ == '__main__':
